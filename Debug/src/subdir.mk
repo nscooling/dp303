0da12637442379229159be8acf7f6ece42ff9597
Niall Cooling <niall.cooling@gmail.com>
2017-03-09 12:06:15 +0000

    ex1

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,16 +4,26 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../src/Filters.cpp \
 ../src/main.cpp 
 
 OBJS += \
+./src/Filters.o \
 ./src/main.o 
 
 CPP_DEPS += \
+./src/Filters.d \
 ./src/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"/home/ubuntu/workspace/DP-303/feabhOS/C/common/inc" -I"/home/ubuntu/workspace/DP-303/feabhOS/C/POSIX/inc" -I"/home/ubuntu/workspace/DP-303/feabhOS/C++/inc" -I"/home/ubuntu/workspace/DP-303/src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 src/main.o: ../src/main.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include "Scheduler.h"
+#include "MessagQueue.h"
+#include "Filters.h"
+
 #include <iostream>
 
 int main()
 {
   feabhOS::Scheduler::init();
 
-  std::cout << "hello world" << std::endl;
+  Generator generator;
+  LowPass   lowpass(5);
+  Display   display;
 
+  FilterBase::queue_type pipe1;
+  FilterBase::queue_type pipe2;
+
+  generator >>= pipe1 >>= lowpass >>= pipe2 >>= display;
+
+  while(true)
+  {
+    generator.run();
+    lowpass.run();
+    display.run();
+  }
 
   feabhOS::Scheduler::start();
   // ----------------------------
